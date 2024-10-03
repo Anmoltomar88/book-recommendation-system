d2cb3ab7f809e4d1bc0fa6eff3cda54b76e2391b
Anmol Tomar <tomaranmol513@gmail.com>
2024-10-03 22:40:00 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+popular_df = pickle.load(open('popular.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           book_name = list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-L'].values),
+                           votes=list(popular_df['num_ratings'].values),
+                           rating=list(popular_df['avg_rating'].values)
+                           )
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books', methods=['POST'])
+def recommend():
+    user_input = request.form.get('user_input').lower()  
+    matching_books = []
+
+    
+    for title in pt.index:
+        if any(word in title.lower() for word in user_input.split()):  
+            matching_books.append(title)
+    
+    if matching_books:
+        
+        index = pt.index.get_loc(matching_books[0])  
+        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:9]
+
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+            data.append(item)
+
+        return render_template('recommend.html', data=data)
+    
+    else:
+        
+        return render_template('recommend.html', message="Our system is still in development and currently only supports 100,000 books in the database.")
+
+if __name__ == '__main__':
+    app.run(debug=True)
